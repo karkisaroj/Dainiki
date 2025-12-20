d1fb0729731c458114062c1037ae7f83220a64fa
saorj karki <karkisaroj3012@gmail.com>
2025-12-20 22:21:45 +0545

    modified components

--- Components/Models/RegisterModel.cs
+++ Components/Models/RegisterModel.cs
@@ -17,8 +17,7 @@ namespace Dainiki.Components.Models
         [Compare("Password", ErrorMessage = "Passwords do not match")]
         public string ConfirmPassword { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Email is required")]
-        [EmailAddress(ErrorMessage = "Invalid email address")]
-        public string Email { get; set; } = string.Empty;
+       
+        
     }
 }
