dc52fe77df62ea5b5c335fff8ffd644d0684da3b
saorj karki <karkisaroj3012@gmail.com>
2026-01-15 15:05:51 +0545

    Updation on navmenu and optimization of theme

---
+++ Components/Models/EntriesModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dainiki.Components.Models
+{
+    public class EntriesModel
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        
+        public string PrimaryMood { get; set; } = string.Empty;
+
+        public List<string?> SecondaryMoods { get; set; } = [];
+
+        public string PhaseOfLife { get; set; } = string.Empty;
+
+        public string Tags { get; set; } = string.Empty;
+
+
+    }
+}
