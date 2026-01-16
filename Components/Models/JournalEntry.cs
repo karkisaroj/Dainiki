326a9549026a9b66db46082dab133ac01025162d
saorj karki <karkisaroj3012@gmail.com>
2026-01-16 17:40:33 +0545

    Journal entries models for static data

---
+++ Components/Models/JournalEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dainiki.Components.Models
+{
+    public class JournalEntry
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public TimeOnly Time { get; set; }
+        public string Title { get; set; } = "";
+        public string Mood { get; set; } = "";
+        public string Preview { get; set; } = "";
+    }
+}
