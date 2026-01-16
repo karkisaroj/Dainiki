0eff5c42029cfcc0fb1a32e7874b420f69c3d9bc
saorj karki <karkisaroj3012@gmail.com>
2026-01-16 23:15:30 +0545

    Entries static model optimization

--- Components/Models/EntriesModel.cs
+++ Components/Models/EntriesModel.cs
@@ -1,24 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Dainiki.Components.Models
+﻿namespace Dainiki.Components.Models
 {
     public class EntriesModel
     {
+        public int Id { get; set; }
+        public DateTime? Date { get; set; }
+        public TimeOnly? Time { get; set; }
+        public string PrimaryMood { get; set; } = string.Empty;
+        public List<string> SecondaryMoods { get; set; } = new List<string>();
+        public string PhaseOfLife { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
-        
-        public string PrimaryMood { get; set; } = string.Empty;
-
-        public List<string?> SecondaryMoods { get; set; } = [];
-
-        public string PhaseOfLife { get; set; } = string.Empty;
-
-        public string Tags { get; set; } = string.Empty;
-
-
+        public string Preview { get; set; } = string.Empty;
+        public List<string> Tags { get; set; } = new List<string>();
     }
 }
