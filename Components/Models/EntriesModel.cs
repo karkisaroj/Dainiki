44ab54b30096817c13384a15174700141af9edc5
saorj karki <karkisaroj3012@gmail.com>
2026-01-18 13:10:01 +0545

    Bug fixes for buttons by disabling the @disable-button

--- Components/Models/EntriesModel.cs
+++ Components/Models/EntriesModel.cs
@@ -5,22 +5,26 @@ public class EntriesModel
     public int Id { get; set; }
     public int UserId { get; set; }
 
-    public DateTime Date { get; set; }= DateTime.Now;
+    public DateTime Date { get; set; } = DateTime.Now;
     public TimeOnly? Time { get; set; } = TimeOnly.FromDateTime(DateTime.Now);
 
-    [Required,StringLength(50)]
+    [Required, StringLength(50)]
     public string Title { get; set; } = "";
 
     [Required]
     public string Content { get; set; } = "";
 
-    [Required,StringLength(100)]
+    [Required(ErrorMessage = "Primary mood is required"), StringLength(100)]
     public string PrimaryMood { get; set; } = "";
     public string SecondaryMoods { get; set; } = "[]";
 
     [StringLength(150)]
-    public string PhaseOfLife { get; set; } = "";
+    public string PhaseOfLife { get; set; } = "[]";
 
-    public string Tags { get; set; } = "[]";            
-    
+    public string Tags { get; set; } = "[]";
+
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
 }
