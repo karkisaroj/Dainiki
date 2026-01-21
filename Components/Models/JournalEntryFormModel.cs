2f7eeba759eba0cb82cdb399259d4d94ceda992f
saorj karki <karkisaroj3012@gmail.com>
2026-01-21 12:26:58 +0545

    Js used as root folder and used in the add entries page

--- Components/Models/JournalEntryFormModel.cs
+++ Components/Models/JournalEntryFormModel.cs
@@ -1,21 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿//using System.ComponentModel.DataAnnotations;
 
-namespace Dainiki.Components.ViewModels
-{
-    public class JournalEntryFormModel
-    {
-        public int UserId { get; set; }
-        public string Title { get; set; } = string.Empty;
+//namespace Dainiki.Components.ViewModels
+//{
+//    public class JournalEntryFormModel
+//    {
+//        public int UserId { get; set; }
+//        public string Title { get; set; } = string.Empty;
 
-        public string Content { get; set; } = string.Empty;
+//        public string Content { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Primary mood is required")]
-        public string PrimaryMood { get; set; } = string.Empty;
+//        [Required(ErrorMessage = "Primary mood is required")]
+//        public string PrimaryMood { get; set; } = string.Empty;
 
-        public HashSet<string> SecondaryMoods { get; set; } = new();
+//        public HashSet<string> SecondaryMoods { get; set; } = new();
 
-        public string PhaseOfLife { get; set; } = string.Empty;
+//        public string PhaseOfLife { get; set; } = string.Empty;
 
-        public string TagsInput { get; set; } = string.Empty; 
-    }
-}
+//        public string TagsInput { get; set; } = string.Empty; 
+//    }
+//}
