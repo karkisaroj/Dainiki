5a0d432d715b3c61a62f9dc5973339c6679d3fd3
saorj karki <karkisaroj3012@gmail.com>
2026-01-25 16:39:52 +0545

    Added constant to store the entry options

---
+++ Components/Shared/Constants/EntryOptions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dainiki.Components.Shared.Constants
+{
+    public static class EntryOptions
+    {
+        public static readonly List<string> PrimaryMoods =
+    [
+        "Happy", "Sad", "Calm", "Angry", "Excited"
+    ];
+
+        public static readonly List<string> SecondaryMoodOptions =
+    [
+        "Work", "Health", "Travel", "Relationships", "Study"
+    ];
+
+        public static readonly List<string> LifePhases =
+    [
+        "School life", "College life", "Work life", "Family life"
+    ];
+
+        public static readonly List<string> PreDefinedTagsOptions =
+    [
+        "Work", "Career", "Studies", "Family", "Friends", "Relationships",
+        "Health", "Fitness", "Personal Growth", "Self-care", "Hobbies", "Travel", "Nature",
+        "Finance", "Spirituality", "Birthday", "Holiday", "Vacation", "Celebration", "Exercise",
+        "Reading", "Writing", "Cooking", "Meditation", "Yoga", "Music", "Shopping", "Parenting",
+        "Projects", "Planning", "Reflection"
+    ];
+    }
+}
